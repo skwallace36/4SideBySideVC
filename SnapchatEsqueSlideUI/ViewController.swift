c8f7198fff1e60af3352918d7733565c0dee2f5e
Stuart Wallace <stuart@Stuarts-MacBook-Pro.local>
2016-12-13 13:06:48 -0500

    Initial Commit

---
+++ SnapchatEsqueSlideUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapchatEsqueSlideUI
+//
+//  Created by Stuart Wallace on 12/13/16.
+//  Copyright © 2016 Stuart Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
